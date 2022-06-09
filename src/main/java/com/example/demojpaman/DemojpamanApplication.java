fcb787300a5e5cdbe2df6e8c5274bd79bb810d48
park sungbum <parksungbum@parks-Mac-Studio.local>
2022-06-09 16:31:44 +0900

    firstman

---
+++ src/main/java/com/example/demojpaman/DemojpamanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojpaman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojpamanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemojpamanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demojpaman/DemojpamanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojpaman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojpamanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
